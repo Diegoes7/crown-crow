f7388a9b3c17102e7cb740ee80c64eff57280577
Diegoes7 <dimitar.i.dimitrov77@gmail.com>
2020-10-13 18:13:46 +0300

    updating stripe integration and updating out favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+import "./stripe-button.styles.scss";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_2oJzefdrHjSiYhYug2ag5czv00ODoswJJF";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successfull");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CROWN-CROW IMPIRE"
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      discription={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
